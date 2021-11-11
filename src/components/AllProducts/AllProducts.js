67d1dc0d03ee7d2a7fff7af500a8ca7a2aec8908
Arvind <arbindudebnath59985@gmail.com>
2021-11-11 15:49:18 +0600

    load data from database and show them into products page and

--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react/cjs/react.development';
+import Footer from '../Home/Footer/Footer';
 import Navbar from '../Home/Navbar/Navbar';
 
 const AllProducts = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/allproducts')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
         <div>
             <Navbar></Navbar>
+            <h1>All Products</h1>
+            <div className="container mt-4 mb-5">
+                <div class="row">
+                    {
+                        products.map(product => <div className='col-sm-6 mb-5 border-bottom'>
+                            <div className="card border-white">
+                                <img className='img-fluid rounded-top' src={product.image} style={{height:'450px', objectFit:'cover'}} alt="" />
+                                <div className="card-body">
+                                    <h5 className="card-title"><u>{product.name}</u></h5>
+                                    <p className="card-text">{product.description.slice(0,120)}...</p>
+                                    <Link to='/purchase'><button className='btn btn-outline-dark'>Purchase</button></Link>
+                                </div>
+                            </div>
+                        </div>)
+                    }
+                </div>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/components/Home/Products/Products.js
+++ src/components/Home/Products/Products.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/allproducts')
+            .then(res => res.json())
+            .then(data => setProducts(data.slice(0,6)))
+    }, [])
+
     return (
         <div>
-            products
+            <h1>Products</h1>
+            <div className="container mt-4 mb-5">
+                <div class="row">
+                    {
+                        products.map(product => <div className='col-sm-4 mb-5 border-bottom'>
+                            <div className="card border-white">
+                                <img className='img-fluid rounded-top' src={product.image} style={{height:'300px', objectFit:'cover'}} alt="" />
+                                <div className="card-body">
+                                    <h5 className="card-title"><u>{product.name}</u></h5>
+                                    <p className="card-text">{product.description.slice(0,50)}...</p>
+                                    <h5 className='mb-3'>From ${product.price}</h5>
+                                    <Link to='/purchase'><button className='btn btn-outline-dark'>Purchase</button></Link>
+                                </div>
+                            </div>
+                        </div>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
+
 
 export default Products;
