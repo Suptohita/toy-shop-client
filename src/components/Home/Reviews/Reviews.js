2172a2b695eb42a9b725354ba4a0578c1c28894b
Arvind <arbindudebnath59985@gmail.com>
2021-11-13 04:46:27 +0600

    user rating handle

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -12,20 +12,32 @@ const Reviews = () => {
             .then(data => setReviews(data))
     }, [])
 
-
     return (
         <div>
             <h3 className='mb-4 mt-3'><u>User's Reviews</u></h3>
             <div className='d-flex container mb-5 user-review'>
                 {
                     reviews.map(review => <div>
-                        <div class="card text-dark mb-2 ms-3 review-bg" style={{width:'18rem', height:'200px'}}>
+                        <div class="card text-dangerdark mb-2 ms-3 review-bg" style={{width:'18rem', height:'200px'}}>
 
                             <div class="card-header">{review.reviewerName}</div>
 
                             <div class="card-body review-text">
-                                <h5 class="card-title">Light card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 class="card-title">{review.topicName} <span className='bg-dark text-warning rounded ps-1 pe-1'>★ {review.rating}</span></h5>
+                                {
+                                    review.rating <= 0 ?
+                                     <div className='text-danger fs-3'>☆☆☆☆☆</div>: 
+                                     review.rating > 0 && review.rating <= 1 ?<div className='text-danger fs-3'>★☆☆☆☆</div>:
+                                     review.rating > 1 && review.rating <= 1.5 ?<div className='text-danger fs-3'>★⯪☆☆☆</div>:
+                                     review.rating > 1.5 && review.rating <= 2 ?<div className='text-danger fs-3'>★★☆☆☆</div>:
+                                     review.rating > 2 && review.rating <= 2.5 ?<div className='text-danger fs-3'>★★⯪☆☆</div>:
+                                     review.rating > 2.5 && review.rating <= 3 ?<div className='text-danger fs-3'>★★★☆☆</div>:
+                                     review.rating > 3 && review.rating <= 3.5 ?<div className='text-danger fs-3'>★★★⯪☆</div>:
+                                     review.rating > 3.5 && review.rating <= 4 ?<div className='text-danger fs-3'>★★★★☆</div>:
+                                     review.rating > 4 && review.rating <= 4.5 ?<div className='text-danger fs-3'>★★★⯪☆</div>:
+                                     <div className='text-dangerdanger fs-3'>★★★★★</div>
+                                }
+                                <p class="card-text">{review.comment}</p>
                             </div>
                         </div>
                     </div>)
